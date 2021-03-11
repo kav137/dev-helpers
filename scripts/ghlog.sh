6eede4f27267860442900c670024fcd558c746ba
Artyom Koval <kav137@users.noreply.github.com>
2021-03-11 09:02:37 +0100

    Add snippets for beautiful github's PR message

---
+++ scripts/ghlog.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Purpose: This snippet allows you to create a beautiful message for github's PR based on the history of your commits
+# Usage: add this file to the globally available ones and run `ghlog NUMBER_OF_COMMITS`, e.g. `ghlog 5`
+
+if [ -z "$1" ]; then
+    echo "Please specify how many commits to log"
+    exit 0
+fi
+
+PURPOSE="Purpose:\n---\nPLACEHOLDER. All the changes described in commit messages (below)\n"
+
+(
+    echo -e "$PURPOSE\nChanges (in chronological order):" && \
+    echo "---" && \
+    (
+        git log -n $1 --pretty=format:"- [%h] %s%n%b" --reverse | \
+        grep "\S"
+    )
+) | pbcopy
+
+echo "Git log for last $1 commits was copied"
